89605399e9dfb33395fdbf17c8d2bacde33cb704
rafainamdar04 <160421004+rafainamdar04@users.noreply.github.com>
2025-06-29 06:03:02 +0530

    version 9

--- src/App.js
+++ src/App.js
@@ -259,8 +259,8 @@ const skillGroups = [
         <div className="nav-links">
           <a href="#about">About</a>
           <a href="#skills">Skills</a>
-          <a href="#certifications">Certifications</a>
           <a href="#projects">Projects</a>
+          <a href="#certifications">Certifications</a>
           <a href="#experience">Experience</a>
           <a href="#internships">Internships</a>
         </div>
